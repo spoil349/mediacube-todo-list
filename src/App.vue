75fe089266e77e099d21bd174c012fca64ca4b38
spoil349 <spoil349@mail.ru>
2022-04-30 22:00:27 +0400

    add: vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
-</style>
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
